be60dedaf78b12b254ccb55d6ac507763111be3a
Константин Бедрицкий <kmb2@tpu.ru>
2020-10-11 23:20:58 +0600

    Draw a triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 
 #include <glad/glad.h>
-// #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
@@ -14,10 +13,18 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource =
     "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos\n"
+    "layout (location = 0) in vec3 aPos;\n"
     ""
     "void main() {\n"
-    "   gl_Position = vec4(aPos, 1.0);\n"
+    "   gl_Position = vec4(aPos, 1.0f);\n"
+    "}\0";
+
+const char *fragmentShaderSource =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    ""
+    "void main() {\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
     "}\0";
 
 int main()
@@ -53,8 +60,62 @@ int main()
         return -1;
     }
 
+    // vertex shader
+    // -------------
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cerr
+            << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+            << infoLog << std::endl;
+    }
+
+    // fragment shader
+    // ---------------
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cerr
+            << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+            << infoLog << std::endl;
+    }
+
+    // shader program
+    // --------------
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cerr
+            << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+            << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
@@ -65,15 +126,29 @@ int main()
         0.5f,
         -0.5f,
         0.0f,
-        0.5f,
+        0.0f,
         0.5f,
         0.0f,
     };
 
     unsigned int VBO;
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
 
     // render loop
     // -----------
@@ -87,6 +162,10 @@ int main()
         // -----
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
