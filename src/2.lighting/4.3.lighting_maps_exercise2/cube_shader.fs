a73d52227d3f8c4bb373d6fa67ceb990d5933b13
Константин Бедрицкий <kmb2@tpu.ru>
2020-10-18 19:53:10 +0600

    4.3.lighting_maps_exercise2

---
+++ src/2.lighting/4.3.lighting_maps_exercise2/cube_shader.fs
@@ -0,0 +1,42 @@
+#version 330 core
+
+in vec3 Normal;
+in vec3 fragPos;
+in vec2 uv;
+
+out vec4 fragColor;
+
+uniform vec3 cameraPosition;
+
+struct Material {
+   sampler2D diffuse;
+   sampler2D specular;
+   float shininess;
+};
+
+struct Light {
+   vec3 position;
+   vec3 ambient;
+   vec3 diffuse;
+   vec3 specular;
+};
+
+uniform Material material;
+uniform Light light;
+
+void main() {
+   vec3 ambient = light.ambient * texture(material.diffuse, uv).rgb;
+
+   vec3 norm = normalize(Normal);
+   vec3 lightDir = normalize(light.position - fragPos);
+   float diff = max(dot(lightDir, norm), 0.0);
+   vec3 diffuse = light.diffuse * (diff * texture(material.diffuse, uv).rgb);
+
+   vec3 viewDir = normalize(cameraPosition - fragPos);
+   vec3 reflectDir = reflect(-lightDir, norm);
+   float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+   vec3 specular = light.specular * (spec * (vec3(1.0) - texture(material.specular, uv).rgb));
+
+   vec3 result = ambient + diffuse + specular;
+   fragColor = vec4(result, 1.0);
+}
